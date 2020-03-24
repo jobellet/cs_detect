2ee9145427748c78724f927ba8b57251c3bb7dba
jobellet <jobellet@gmail.com>
2020-03-24 21:46:42 +0100

    bigger_random_intervals

--- CS.py
+++ CS.py
@@ -26,7 +26,7 @@ import umap
 import hdbscan
 
 # to plot things
-from matplotlib import pyplot as plt 
+from matplotlib import pyplot as plt
 from matplotlib.pyplot import *
 matplotlib.style.use('classic')
 matplotlib.rcParams['pdf.fonttype'] = 42
@@ -53,7 +53,7 @@ def concatenate_segments(LFP,HIGH,Interval_inspected,Labels):
     return (compLFP,compHIGH,compLabels)
 
 def get_field_mat(data,fields): # go through structure in a .mat file to find the variable
-    
+
     if len(fields) == 0:
         return([])
     for i,field in enumerate(fields):
@@ -61,15 +61,15 @@ def get_field_mat(data,fields): # go through structure in a .mat file to find th
     while len(data)==1:
         data = data[0]
     return(data)
-            
+
 def get_field_pkl(df,field): # get the variable in a panda dataframe
     if field != None:
         data = np.array(df[field])
     else:
         data = None
     return(data)
-  
-  
+
+
 def list2array(mylist): # convert a list into an array of segments of equal length
     if len(mylist) > 0:
         types = [hasattr(mylist[i],"__len__") for i in range(len(mylist))]
@@ -87,9 +87,9 @@ def list2array(mylist): # convert a list into an array of segments of equal leng
     else:
         myarray = []
 
-    
+
     return(myarray)
-  
+
 
 def butter_bandpass(lowcut, highcut, fs, order=5):
     nyq = 0.5 * fs
@@ -100,16 +100,16 @@ def butter_bandpass(lowcut, highcut, fs, order=5):
 
 def norm_LFP(LFP,sampling_freq): # normalise the LFP for the network
     if len(LFP) > 0:
-        
+
         types = [hasattr(LFP[i],"__len__") for i in range(len(LFP))]
         if all(types) == 0 : #surely a vector
             LFP = np.asarray(LFP)
-            
+
             b, a = butter_bandpass(30, 400, sampling_freq, order = 2)
 
-            LFP =  signal.filtfilt(b, a, np.double(np.asarray(LFP))) 
+            LFP =  signal.filtfilt(b, a, np.double(np.asarray(LFP)))
             LFP /= np.median(np.abs(LFP))
-            
+
 
         else:
             # normalise LFP as a multiple of absolute median
@@ -117,26 +117,26 @@ def norm_LFP(LFP,sampling_freq): # normalise the LFP for the network
             LFP = [signal.filtfilt(b, a, np.double(np.asarray(i))) for i in LFP ]
             LFP = [i/np.median(np.abs(i)) for i in LFP ]
     else:
-        LFP = []     
+        LFP = []
     return(LFP)
 
 def norm_high_pass(high_pass): # normalise the high pass signal for the network
     if len(high_pass) > 0:
-        
+
         types = [hasattr(high_pass[i],"__len__") for i in range(len(high_pass))]
         if all(types) == 0 : #surely a vector
             high_pass = np.asarray(high_pass)
             high_pass = np.double(high_pass)
-            high_pass /= np.median(np.abs(high_pass)) 
+            high_pass /= np.median(np.abs(high_pass))
 
         else:
             # normalise high_pass as a multiple of absolute median
             high_pass = [i/np.median(np.abs(i)) for i in high_pass ]
     else:
-        high_pass = []     
+        high_pass = []
     return(high_pass)
 
-       
+
 def load_data(filename = [],field_LFP = [],field_high_pass = [], field_label = [],field_intervs = [],sampling_freq = 25000):
     # loads the data for .mat .pkl or .csv
     if len(filename) == 0:
@@ -174,7 +174,7 @@ def save_data(output_file,labels):
                 temp1.append(labels[keys[j]][i])
             temp = pd.DataFrame([temp1], columns=keys)
             df = df.append(temp)
-    if file_extension == '.pkl': 
+    if file_extension == '.pkl':
         df.to_pickle(output_file)
     elif file_extension == '.mat':
         io.savemat(output_file,labels)
@@ -182,7 +182,7 @@ def save_data(output_file,labels):
         df.to_csv(output_file)
     elif file_extension == '.h5' :
         df.to_hdf(output_file, key='df', mode='w')
-        
+
 def nothingfound(output_name): # when no CS are found
     print('Warning : no complex spikes was found. Check your signals')
     labels = {'cs_onset': [],
@@ -192,7 +192,7 @@ def nothingfound(output_name): # when no CS are found
     if output_name != None:
         print('saving '+output_name)
         save_data(output_name,labels)
-    return (labels)   
+    return (labels)
 
 def create_random_intervals(sampling_rate,LFP,Labels):
     new_label = np.zeros_like(LFP).astype(bool);
@@ -203,30 +203,30 @@ def create_random_intervals(sampling_rate,LFP,Labels):
     else:
         onsets = Labels[:,0]
         offset = Labels[:,1]
-    
+
     for o,e in zip(onsets,offsets):
             new_label[o:e] = True
-            shifto=np.random.randint(sampling_rate/10)
-            shifte=np.random.randint(sampling_rate/10)
-            Interval_inspected[o-shifto:e+shifte] = True
+            shifto = np.random.randint(sampling_rate/5)
+            shifte = np.random.randint(sampling_rate/5)
+            Interval_inspected[np.max(0,o-shifto):np.min(e+shifte,len(LFP)-1)] = True
     return(Interval_inspected)
 def detect_CS(weights_name, LFP, High_passed, output_name = None,  sampling_frequency = 25000, ks=9,mp=7, exlude_w = 3,realign = True, alignment_w = (-.5,2), cluster = True, cluster_w = (-2,2),plot = False, plot_w= (-4,8),plot_only_good = True):
     # important arguments:
     # -filename is the filename path. If it is not defined then you should input the LFP and the High-passed signal
     # -weights_name is the path of the weight or just the name of the weight if it is stored in /training
     # -sampling_frequency is the sampling frequency of the signal analysed
-    # ks and mp are the size of the kernel and max pooling operations respectively. 
+    # ks and mp are the size of the kernel and max pooling operations respectively.
     if plot:
         cmap = plt.get_cmap('jet')
 
     samp  = int(sampling_frequency/1000) # Khz
-    
+
     if len(LFP)==0 or len(High_passed)==0: # if one signal is missing abort
         labels = nothingfound(output_name)
         return(labels)
-    
+
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    
+
     trial_length = 1 #sec, length per "trial"
     trial_length *= int(samp*1000)
     overlap = 0.1 #sec, length of overlap
@@ -234,8 +234,8 @@ def detect_CS(weights_name, LFP, High_passed, output_name = None,  sampling_freq
     if np.max(LFP.shape)>trial_length:
         # preprocessing: cut recording into overlapping pieces because the network can't deal with really big signals
         total_length = np.max(LFP.shape) #total recording length
-        
-        
+
+
         if np.floor(total_length/(trial_length-overlap))<(total_length-overlap)/(trial_length-overlap):
             num_steps = int(np.floor(total_length/(trial_length-overlap)))
         else:
@@ -243,15 +243,15 @@ def detect_CS(weights_name, LFP, High_passed, output_name = None,  sampling_freq
         steps = np.array([i*trial_length-i*overlap for i in range(num_steps)]).astype(int)
         LFP_mat = np.zeros((num_steps+1,trial_length))
         High_mat = np.zeros((num_steps+1,trial_length))
-        
+
         for i,s in enumerate(steps):
             LFP_mat[i,:] = LFP[s:s+int(trial_length)]
             High_mat[i,:] = High_passed[s:s+int(trial_length)]
-            
+
         # exception for last piece (from end-trial_length to end)
         LFP_mat[-1,:] = LFP[-int(trial_length):]
         High_mat[-1,:] = High_passed[-int(trial_length):]
-        
+
         # normalise LFP as a multiple of absolute median
         for i in np.arange(0,np.shape(LFP_mat)[0]):
             LFP_mat[i,:] = LFP_mat[i,:]-np.median(LFP_mat[i,:])
@@ -260,7 +260,7 @@ def detect_CS(weights_name, LFP, High_passed, output_name = None,  sampling_freq
         LFP_mat = LFP
         High_mat = High_passed
     # U'n'Eye
-    
+
     model = uneye.DNN(ks=ks,mp=mp,weights_name=weights_name,sampfreq=sampling_frequency,min_sacc_dur=1,doDiff = False)
 
     if device.type == 'cpu': # if no GPU, loop through every segments
@@ -268,13 +268,13 @@ def detect_CS(weights_name, LFP, High_passed, output_name = None,  sampling_freq
         Prob = np.zeros((LFP_mat.shape[0],2,trial_length))
 
         for i in range(LFP_mat.shape[0]):
-            Pred[i,:],Prob[i,:,:] = model.predict(LFP_mat[i,:],High_mat[i,:]) 
+            Pred[i,:],Prob[i,:,:] = model.predict(LFP_mat[i,:],High_mat[i,:])
         LFP_mat = None
         High_mat = None
-        
+
     else: # if GPU, process all segments at ones
         Pred,Prob = model.predict(LFP_mat,High_mat)
-    
+
     if np.max(LFP.shape)>trial_length:
         # recover original shape
         Prediction = np.zeros(total_length)
@@ -305,20 +305,20 @@ def detect_CS(weights_name, LFP, High_passed, output_name = None,  sampling_freq
                    'cs_offset':cs_offset}
         if output_name != None:
             print('saving '+output_name)
-            save_data(output_name,labels) 
+            save_data(output_name,labels)
         return(labels)
-    
+
     alignment_window = (np.array([sampling_frequency*alignment_w[0],sampling_frequency*alignment_w[1]])/1000).astype(int) # 2 ms to realign CS onset
     cluster_window = (np.array([sampling_frequency*cluster_w[0],sampling_frequency*cluster_w[1]])/1000).astype(int)  # consider the first 2 ms after CS onset for clustering
     plot_window = (np.array([sampling_frequency*plot_w[0],sampling_frequency*plot_w[1]])/1000).astype(int) # plot between 4 ms before and 8 ms after CS onset
-    
+
     veto_wind = sampling_frequency*[0,0]
     if cluster:
         veto_wind =  [min(veto_wind[0],cluster_window[0]),max(veto_wind[1],cluster_window[1])]
     if realign:
         veto_wind =  [min(veto_wind[0],alignment_window[0]),max(veto_wind[1],alignment_window[1])]
     veto_wind = [min(plot_window[0],veto_wind[0]),max(plot_window[1],veto_wind[1])]
-    
+
     # remove CSs detected to close from the edges of the signal
     not_too_close = np.logical_and((cs_onset>-veto_wind[0]),(cs_offset<len(Prediction)-veto_wind[1]))
     cs_offset = cs_offset[not_too_close]
@@ -327,52 +327,52 @@ def detect_CS(weights_name, LFP, High_passed, output_name = None,  sampling_freq
     if len(cs_onset) == 0:
         labels = nothingfound(output_name)
         return(labels)
-    ######################################## 
+    ########################################
     # post processing
     ########################################
-    
-    
+
+
     # align each complex spike to the overall average
-    
+
     average_CS = np.zeros((len(cs_onset),alignment_window[1]-alignment_window[0]))
     for i,j in enumerate(cs_onset):
         average_CS[i,:]=High_passed[j+alignment_window[0]:j+alignment_window[1]]
-    
+
     norm_signal = np.median(np.abs(High_passed))/0.6745
     corrected_on=np.zeros((len(cs_onset),1))
-    
+
     if realign:
         for i,j in enumerate(cs_onset):
             c=np.correlate(np.mean(average_CS,axis=0),average_CS[i,:],"full")
             lag = (np.argmax(c)-c.size/2)+.5
             corrected_on[i]=int(j-lag)
-    else:     
+    else:
         corrected_on = cs_onset
-      
+
     if cluster == False: # if only the realignment of onsets was needed
         cs_onset = corrected_on
         labels = {'cs_onset':cs_onset,
                  'cs_offset':cs_offset}
         if output_name != None:
             print('saving '+output_name)
-            save_data(output_name,labels) 
+            save_data(output_name,labels)
         return(labels)
-    
-    average_CS2 = np.zeros((len(cs_onset),cluster_window[1]-cluster_window[0])) 
+
+    average_CS2 = np.zeros((len(cs_onset),cluster_window[1]-cluster_window[0]))
     for i,j in enumerate(corrected_on):
         ind = int(j)
         average_CS2[i,:] = High_passed[ind+cluster_window[0]:ind+cluster_window[1]]
-    
+
     average_prob = np.zeros((len(cs_onset),plot_window[1]-plot_window[0]))
     for i,j in enumerate(corrected_on):
         ind = int(j)
         average_prob[i,:] = Probability[ind+plot_window[0]:ind+plot_window[1]]
-        
+
     average_LFP = np.zeros((len(cs_onset),cluster_window[1]-cluster_window[0]))
     for i,j in enumerate(corrected_on):
         ind = int(j)
         average_LFP[i,:] = LFP[ind+cluster_window[0]:ind+cluster_window[1]]
-        
+
     if plot:
         average_CS_plot = np.zeros((len(cs_onset),plot_window[1]-plot_window[0]))
         for i,j in enumerate(cs_onset):
@@ -386,8 +386,8 @@ def detect_CS(weights_name, LFP, High_passed, output_name = None,  sampling_freq
         for i,j in enumerate(corrected_on):
             ind = int(j)
             average_LFP_plot[i,:] = LFP[ind+plot_window[0]:ind+plot_window[1]]
-        
-        
+
+
         u = np.random.permutation(i)[:np.round(i/2).astype(int)]
         fig = plt.figure()
         ax1 = plt.subplot(411)
@@ -402,24 +402,24 @@ def detect_CS(weights_name, LFP, High_passed, output_name = None,  sampling_freq
     if np.shape(average_CS2)[0]<n_neighbors: # exit if the number of complex spike detected is too small
         labels = nothingfound(output_name)
         return(labels)
-    
-    embedding = umap.UMAP(n_neighbors = n_neighbors , min_dist=0.00001) 
+
+    embedding = umap.UMAP(n_neighbors = n_neighbors , min_dist=0.00001)
     embedding = embedding.fit_transform(np.concatenate((average_CS2, average_LFP), axis=1))
 
     # clustering
     clustering = hdbscan.HDBSCAN(allow_single_cluster=True)
     clustering = clustering.fit(embedding)
-    
-    
+
+
     # keep all cluster of waveforms for which the average predictive probability lasts at least "exlude_w" ms
-      
+
     tree = clustering.single_linkage_tree_.to_pandas()
     penultimate = np.sort(tree['size'])[-3]
     last = np.sort(tree['size'])[-2]
     distance =np.concatenate((np.array(tree['distance'][tree['size']==penultimate]), np.array(tree['distance'][tree['size']==last])), axis=None).mean()
     labels_big = clustering.single_linkage_tree_.get_clusters(distance,min_cluster_size=0)
-    
-    
+
+
     include = np.zeros((1,len(cs_onset))).astype(bool)[0]
     good_CS = []
     if plot:
@@ -442,7 +442,7 @@ def detect_CS(weights_name, LFP, High_passed, output_name = None,  sampling_freq
                         ax0.plot(np.arange(plot_window[0],plot_window[1])/sampling_frequency*1000,average_LFP_plot[z[0],:],c = colors[i], Linewidth = 0.1)
                         ax0.plot(np.arange(plot_window[0],plot_window[1])/sampling_frequency*1000,
                                   np.mean(average_LFP_plot[labels_big==lab,:],axis = 0),c = colors[i])
-                   
+
                     ax1.plot(np.arange(plot_window[0],plot_window[1])/sampling_frequency*1000,
                              np.mean(average_CS2_plot[labels_big==lab,:],axis = 0),c = colors[i])
                     ax2.plot(np.arange(plot_window[0],plot_window[1])/sampling_frequency*1000,np.mean(average_prob[labels_big==lab,:],axis = 0), c = colors[i])
@@ -452,15 +452,15 @@ def detect_CS(weights_name, LFP, High_passed, output_name = None,  sampling_freq
                 for z in u:
                     ax1.plot(np.arange(plot_window[0],plot_window[1])/sampling_frequency*1000,average_CS2_plot[z[0],:],c = colors[i],Linewidth = 0.1)
                     ax0.plot(np.arange(plot_window[0],plot_window[1])/sampling_frequency*1000,average_LFP_plot[z[0],:],c = colors[i],Linewidth = 0.1)
-                
+
                 ax0.plot(np.arange(plot_window[0],plot_window[1])/sampling_frequency*1000,
                          np.mean(average_LFP_plot[labels_big==lab,:],axis = 0),c = colors[i])
                 ax1.plot(np.arange(plot_window[0],plot_window[1])/sampling_frequency*1000,
                          np.mean(average_CS2_plot[labels_big==lab,:],axis = 0),c = colors[i])
                 ax2.plot(np.arange(plot_window[0],plot_window[1])/sampling_frequency*1000,
                          np.mean(average_prob[labels_big==lab,:],axis = 0),c = colors[i])
-                  
-    if plot:    
+
+    if plot:
         xlabel('time from CS onset (ms)',fontsize=15)
         ylabel('Voltage (uV)',fontsize=15)
         ax1.spines['top'].set_visible(False)
@@ -480,9 +480,9 @@ def detect_CS(weights_name, LFP, High_passed, output_name = None,  sampling_freq
                 plt.scatter(embedding[labels_big==lab,0],embedding[labels_big==lab,1],c = colors[i], edgecolors = 'face')
     cs_onset = corrected_on[include];
     cs_offset = cs_offset[include]
-    
+
     cs_onset = cs_onset.astype('int')
-    try: 
+    try:
         cs_onset = np.concatenate(cs_onset)
     except: # some times no complex spikes end up being detected
         labels = nothingfound(output_name)
@@ -497,6 +497,3 @@ def detect_CS(weights_name, LFP, High_passed, output_name = None,  sampling_freq
         print('saving '+output_name)
         save_data(output_name,labels)
     return(labels)
-    
-    
-    
